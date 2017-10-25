15e2a25cf5d9bd208bac4149e8da2e5c9927b227
Valerij Kozlov <valerij.kozlov@gmail.com>
2017-10-25 10:13:15 +0100

    Test for search of top 5 repositories

---
+++ src/GitHubInfo.Tests/SearchTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace GitHubInfo.Tests
+{
+    [TestFixture]
+    class SearchTests
+    {
+        [Test]
+        public void SearchByStringReturnsTop5Results()
+        {
+            var testString = "repo";
+            var search = new Search();
+
+            var results = search.LookUpTopMatches(testString, 5);
+            Assert.That(results.Count(), Is.EqualTo(5));
+        }
+    }
+}
